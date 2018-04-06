ce7b6713009cb96f207ffb8f62849c978a647e67
SuneJepsen <kongsune@hotmail.com>
2018-04-06 14:33:13 +0200

    Added a Library.Job and a WebApi

---
+++ WebApi.Access/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApi.Access.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApi.Access/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi.Access.WebApiApplication" Language="C#" %>
