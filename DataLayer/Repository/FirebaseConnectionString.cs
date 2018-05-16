da8c510813bc963f9b4fbfd61d244fc19795fbc6
SuneJepsen <kongsune@hotmail.com>
2018-05-16 09:17:37 +0200

    test

--- DataLayer/Repository/FirebaseConnectionString.cs
+++ DataLayer/Repository/FirebaseConnectionString.cs
@@ -6,5 +6,12 @@
         public readonly static string CopyData = "https://copy-raw-data-floor.firebaseio.com/{0}.json";
         public readonly static string InferredData = "https://inferred-data.firebaseio.com/{0}.json";
         public readonly static string HandShakeData = "https://handshake-data.firebaseio.com/{0}.json";
+
+         
+
+
+        // https://raw-data-ed89f.firebaseio.com/
+
+        // https://copy-data.firebaseio.com/
     }
 }
